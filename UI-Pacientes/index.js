4237135e734cb380b56f5ec42770ce99916c0d84
Alejandro Londoño <97139311+alejolondo@users.noreply.github.com>
2022-09-22 14:41:51 -0500

    Agregué los archivos de la UI-de registro hechos con HTML-JS

---
+++ UI-Pacientes/index.js
@@ -0,0 +1,62 @@
+
+
+document.getElementById('registrar').addEventListener("click", () => {
+    const nombre = document.getElementById("primer-nombre").value
+    const segundoNombre = document.getElementById("segundo-nombre").value
+    const apellido = document.getElementById("primer-apellido").value
+    const segundoApellido = document.getElementById("segundo-apellido").value
+    const tipoDocumento = document.getElementById('tipo-documento').value
+    const numeroDocumento = document.getElementById('numero-documento').value
+    const numeroCelular = document.getElementById('numero-celular').value
+    const factores = document.getElementById('factores').value
+    let factorId = ''
+    const idPaciente = ''
+    
+
+    if (factores === 'Depresion'){
+        factorId === 1
+    }
+    else  {
+        factorId === 2
+    } 
+
+    
+
+
+console.log(factorId)
+
+const datosPaciente = {
+            paciente: {
+                pacienteId: 0,
+                primerNombre: nombre,
+                segundoNombre: segundoNombre,
+                primerApellido: apellido,
+                segundoApellido: segundoApellido,
+                tipoDocumento: tipoDocumento,
+                numeroDocumento: numeroDocumento,
+                numeroCelular: numeroCelular,
+            },
+            factor: {
+                factorId: 1,
+                nombre: factores
+            }
+    
+          }
+          
+          const options = {
+            method: "POST",
+            mode: "cors",
+            body: JSON.stringify(datosPaciente),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8", 'Access-Control-Allow-Origin':'*'
+            }
+        }
+        
+        
+        fetch("https://tdsbotapi-v01.azurewebsites.net/api/Conversacion/Registro", options)
+        .then(response => response.json())
+        .then(data => console.log(data))
+           
+ })
+    
+    
