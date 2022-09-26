3dbc8beb8eff2293b30e74ceab28fb6b157eeb5e
Alejandro Londoño <97139311+alejolondo@users.noreply.github.com>
2022-09-26 17:00:17 -0500

    Estilicé la tabla - Actualicé cambios

--- UI-Consulta/index.js
+++ UI-Consulta/index.js
@@ -1,82 +1,89 @@
 const btnConsulta = document.getElementById('btn-consulta')
-const fechaInicio = document.getElementById('hora-inicio').value 
-const fechaFinal = document.getElementById('hora-final').value
 
-btnConsulta.addEventListener("click", ()=>{
+btnConsulta.addEventListener("click", () => {
 
-    
+    const fechaInicio = document.getElementById('hora-inicio').value
+    const fechaFinal = document.getElementById('hora-final').value
+
     const datos = {
-            fechaInicio: fechaInicio,
-            fechaFin: fechaFinal
-          
+        fechaInicio: fechaInicio,
+        fechaFin: fechaFinal
+
     }
 
-function traerdatos(data){
-    
+    function traerdatos(data) {
+
         let html = ` 
 
-        <section id="encabezado">
-          <p>Nombres</p>
-          <p>Apellidos</p>
-          <p>Identificación</p>
-          <p>Factor</p>
-          <p>Puntaje</p>
-          <p>Fecha de registro</p>
-      
-        </section>
+        <table>
+                <tr class="encabezado" >
+                    <td>Nombre</td>
+                    <td>Apellidos</td>
+                    <td>Identificación</td>
+                    <td>Factor</td>
+                    <td>Puntaje</td>
+                    <td>Fecha Registro</td>
+
+                </tr>
+                
         `
-        
+
 
         for (let dato of data) {
-            
-        if(dato.esAlerta){
-                html += `
-              
-        <section class = "tuplaDiferente">
-              <p>${dato.nombres}</p>
-              <p>${dato.apellidos}</p>
-              <p>${dato.identificacion}</p>
-              <p>${dato.factor}</p>
-              <p>${dato.puntaje}</p>
-              <p>${dato.fechaRgistro}</p>
 
-        </section> `   
-            }else{
+            if (dato.esAlerta) {
+                html += `
+                            
+                <tr class = "tuplaDiferente">
+                    <td>${dato.nombres}</td>
+                    <td>${dato.apellidos}</td>
+                    <td>${dato.identificacion}</td>
+                    <td>${dato.factor}</td>
+                    <td>${dato.puntaje}</td>
+                    <td>${dato.fechaRegistro.slice(0,10)}</td>
+
+                </tr>
+                `
+            } else {
                 html += `
                   
-                <section class = "tupla">
-                      <p>${dato.nombres}</p>
-                      <p>${dato.apellidos}</p>
-                      <p>${dato.identificacion}</p>
-                      <p>${dato.factor}</p>
-                      <p>${dato.puntaje}</p>
-                      <p>${dato.fechaRgistro}</p>
-    
-                </section>    
+                
+                <tr >
+                    <td>${dato.nombres}</td>
+                    <td>${dato.apellidos}</td>
+                    <td>${dato.identificacion}</td>
+                    <td>${dato.factor}</td>
+                    <td>${dato.puntaje}</td>
+                    <td>${dato.fechaRegistro.slice(0,10)}</td>
+
+                </tr>
+                  
                     `
             }
 
         }
 
-        
-       
+
+
         document.getElementById('datos-consulta').innerHTML = html
-    
-}
+
+    }
 
 
-    
+
     const options = {
-         method: "POST",            
+        method: "POST",
         body: JSON.stringify(datos),
         headers: {
-        "Content-Type": "application/json",
-		"Accept": "application/json"
+            "Content-Type": "application/json",
+            "Accept": "application/json"
         }
-        }
-        
-        
-        fetch("https://tdsbotapi-v01.azurewebsites.net/api/Consultas/Consulta", options)
+    }
+
+
+    fetch("https://tdsbotapi-v01.azurewebsites.net/api/Consultas/Consulta", options)
         .then(response => response.json())
         .then(data => traerdatos(data))
 })
+
+// console.log(data) .then(data => traerdatos(data))
